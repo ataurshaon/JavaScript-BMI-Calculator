80da35d4d1bb7af079743b98239f142c6981f747
ataurshaon <shaonn22@gmail.com>
2022-02-26 17:21:27 +0600

    BMI Calculator First Commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const heightInput = document.getElementById('height-input');
+const weightInput = document.getElementById('weight-input');
+const successButton = document.getElementById('btn-success');
+const resetButton = document.getElementById('btn-reset');
+const result = document.getElementById('dynamic__bmi');
+const message = document.getElementById('bmi_text');
+
+function calculateBMI(){
+    let height, weight, bmi;
+    height = Number(heightInput.value);
+    weight = Number(weightInput.value);
+    bmi = weight / (height * 0.0254 * height * 0.0254);
+    result.textContent = bmi.toFixed(2);
+    let msg = showMessage(bmi);
+    message.textContent = msg;
+}
+function showMessage(bmiValue){
+        if(bmiValue < 16){
+            return "You are Severe Thin"
+        } else if(bmiValue >= 16 && bmiValue <= 17){
+            return "You are Moderate Thin"
+        } else if(bmiValue > 17 && bmiValue <= 18.5){
+            return "You are Mid Thin"
+        } else if(bmiValue > 18.5 && bmiValue <= 25){
+            return "You are Normal"
+        } else if(bmiValue > 25){
+            return "You are Overweight"
+        }
+    }
+
+function reset(){
+    heightInput.value = '';
+    weightInput.value = '';
+    result.textContent = '_________';
+    message.textContent = '';
+}
+
+function eventHandler(){
+    if(Number(heightInput.value) && Number(weightInput.value)){
+        calculateBMI();
+    }else{
+        alert('Please provide valid inputs');
+    }
+    reset();
+}
+
+successButton.addEventListener('click', eventHandler);
+resetButton.addEventListener('click', reset);
